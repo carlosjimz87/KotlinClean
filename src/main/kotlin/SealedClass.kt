39eb071fc55063e444df94a7ad9e4e590d9d1ca9
Carlos Jiménez <carlosjimz87@gmail.com>
2021-10-02 19:21:33 +0200

    complex sealed class

--- src/main/kotlin/SealedClass.kt
+++ src/main/kotlin/SealedClass.kt
@@ -1,6 +1,7 @@
 /**
  * `Sealed classes` allow to set a specific number of states,
- * plus more capabilities than `Enums`
+ * plus more capabilities than `Enums`.
+ * (Know states ad compile time, avoid typos and get restrictive responses to states by using when.)
  */
 sealed class Result<out T : Any>(val data: Any? = null, val message: String? = null) {
     class Success<out T : Any>(data: T) : Result<T>(data)
@@ -12,6 +13,20 @@ sealed class Result<out T : Any>(val data: Any? = null, val message: String? = n
 val <T> T.exhaustive: T
     get() = this
 
+
+/**
+ * A more complex `Sealed classes` to handle multiple states with complex nested classes
+ */
+sealed class ComplexResult<out K : Any> {
+    data class Success<out K : Any>(val data: K) : ComplexResult<K>()
+    sealed class Error(val exception: Exception) : ComplexResult<Nothing>() {
+        class Recoverable(exception: Exception) : ComplexResult.Error(exception)
+        class NonRecoverable(exception: Exception) : ComplexResult.Error(exception)
+    }
+
+    object InProgress : ComplexResult<Nothing>()
+}
+
 fun main() {
 
     listOf(
@@ -22,6 +37,19 @@ fun main() {
     )
         .forEach {
             handleResult(it)
+        }
+
+
+    listOf(
+        ComplexResult.InProgress,
+        ComplexResult.Success(1),
+        ComplexResult.InProgress,
+        ComplexResult.Error.NonRecoverable(Exception("Non recoverable error. Bye!")),
+        ComplexResult.InProgress,
+        ComplexResult.Error.Recoverable(Exception("Recoverable error. Retry!"))
+    )
+        .forEach {
+            handleComplexResult(it)
         }
 }
 
@@ -35,6 +63,24 @@ fun handleResult(result: Result<Int>) {
         }
         Result.Loading -> {
             println("Loading...")
+        }
+    }.exhaustive
+}
+
+
+fun handleComplexResult(result: ComplexResult<Int>) {
+    when (result) {
+        is ComplexResult.Error.NonRecoverable -> {
+            result.exception.printStackTrace()
+        }
+        is ComplexResult.Error.Recoverable -> {
+            result.exception.printStackTrace()
+        }
+        is ComplexResult.Success -> {
+            println("Success with: ${result.data}")
+        }
+        ComplexResult.InProgress -> {
+            println("In progress...")
         }
     }.exhaustive
 }
